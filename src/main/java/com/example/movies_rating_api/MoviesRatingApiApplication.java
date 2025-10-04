572977fb26bef06cec3cded72f1a6054040be01e
QVSD <gameruldragos@gmail.com>
2025-10-04 11:51:01 +0300

    Setup prepared

---
+++ src/main/java/com/example/movies_rating_api/MoviesRatingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.movies_rating_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRatingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRatingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movies_rating_api/MoviesRatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movies_rating_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

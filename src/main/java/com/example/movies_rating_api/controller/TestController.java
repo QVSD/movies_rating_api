8450a04d46a3eb6500246729548c606059a41834
QVSD <gameruldragos@gmail.com>
2025-10-04 12:05:24 +0300

    Setup initialised, A Controller for testing purposes and Security Config for endpoint accesing were created, they are going to be removed in the future commits

---
+++ src/main/java/com/example/movies_rating_api/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.movies_rating_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Setup is OK champ";
+    }
+}
